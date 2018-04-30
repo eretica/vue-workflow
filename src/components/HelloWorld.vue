01fcc388fa3a5690304f4c68ee1e64bd26c3eeaa
eretica <eretica.m@gmail.com>
2018-04-30 19:43:20 +0900

    Add test drag & drop

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,20 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div id="mydiv"
+         @mousedown="dragMouseDown"
+         @mouseup="dragMouseUp"
+         v-bind:style="{top: position.top + 'px' , left: position.left + 'px'}">
+      <div id="mydivheader">
+        Content
+        <div>
+          <input type="text">
+        </div>
+        <div>
+          <button>👀</button>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -88,26 +23,54 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      offsetX: 0,
+      offsetY: 0,
+      position: {
+        top: '500',
+        left: '0',
+      },
+      dragg: false
     }
+  },
+  created: function () {
+    window.addEventListener('mousemove', this.elementDrag);
+  },
+  methods: {
+    dragMouseDown: function (e) {
+      this.offsetX = e.clientX - this.position.left;
+      this.offsetY = e.clientY - this.position.top;
+      this.dragg = true;
+    },
+    dragMouseUp: function (e) {
+      this.dragg = false
+    },
+    elementDrag: function (e) {
+      if (!this.dragg) {
+        return;
+      }
+      this.position.top = e.clientY - this.offsetY;
+      this.position.left = e.clientX - this.offsetX;
+    },
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+
+#mydiv {
+  position: absolute;
+  z-index: 9;
+  background-color: #f1f1f1;
+  border: 1px solid #d3d3d3;
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+#mydivheader {
+  padding: 10px;
+  cursor: move;
+  z-index: 10;
+  background-color: #2196F3;
+  color: #fff;
 }
 </style>
