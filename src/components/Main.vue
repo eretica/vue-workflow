0b23372d5de6d780a5684cd76414684f95684da8
eretica <eretica.m@gmail.com>
2018-06-05 21:48:56 +0900

    Remove useless code

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -17,35 +17,11 @@ export default {
   components: { BoxComponent },
   template: '<box-component>',
   data () {
-    return {
-      offsetX: 0,
-      offsetY: 0,
-      position: {
-        top: '500',
-        left: '0',
-      },
-      dragg: false
-    }
+    return {};
   },
   created: function () {
-    window.addEventListener('mousemove', this.elementDrag);
   },
   methods: {
-    dragMouseDown: function (e) {
-      this.offsetX = e.clientX - this.position.left;
-      this.offsetY = e.clientY - this.position.top;
-      this.dragg = true;
-    },
-    dragMouseUp: function (e) {
-      this.dragg = false
-    },
-    elementDrag: function (e) {
-      if (!this.dragg) {
-        return;
-      }
-      this.position.top = e.clientY - this.offsetY;
-      this.position.left = e.clientX - this.offsetX;
-    },
   }
 }
 </script>
@@ -53,19 +29,19 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-#mydiv {
-  position: absolute;
-  z-index: 9;
-  background-color: #f1f1f1;
-  border: 1px solid #d3d3d3;
-  text-align: center;
-}
+/*#mydiv {*/
+  /*position: absolute;*/
+  /*z-index: 9;*/
+  /*background-color: #f1f1f1;*/
+  /*border: 1px solid #d3d3d3;*/
+  /*text-align: center;*/
+/*}*/
 
-#mydivheader {
-  padding: 10px;
-  cursor: move;
-  z-index: 10;
-  background-color: #2196F3;
-  color: #fff;
-}
+/*#mydivheader {*/
+  /*padding: 10px;*/
+  /*cursor: move;*/
+  /*z-index: 10;*/
+  /*background-color: #2196F3;*/
+  /*color: #fff;*/
+/*}*/
 </style>
