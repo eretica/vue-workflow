6b10d06f2c90c7467215be2999aaa6a039b9e592
eretica <eretica.m@gmail.com>
2018-06-06 00:59:45 +0900

    Fix load module path

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,21 +12,21 @@
 </template>
 
 <script>
-  import BoxComponent from './box.vue';
+  import BoxComponent from '@/components/Box.vue'
 
-export default {
-  name: 'HelloWorld',
-  components: { BoxComponent },
-  template: '<box-component>',
-  data () {
-    return {count: 1};
-  },
-  methods: {
-    add_component: function () {
-      this.count++;
+  export default {
+    name: 'HelloWorld',
+    components: { BoxComponent },
+    template: '<box-component>',
+    data () {
+      return {count: 1};
+    },
+    methods: {
+      add_component: function () {
+        this.count++;
+      }
     }
   }
-}
 </script>
 
 <style scoped>
