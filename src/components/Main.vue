85da7f5f9d6adf4bff09b9b1841a1fa4d9e011fe
eretica <eretica.m@gmail.com>
2018-06-05 21:57:28 +0900

    Fix component add when button click

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="hello">
+    <button @click="add_component">Add Component</button>
 
-    <box-component></box-component>
-    <box-component></box-component>
-    <box-component></box-component>
+    <box-component v-for="n in count"></box-component>
 
   </div>
 </template>
 
 <script>
-  // import MyComponent from './test-component.vue';
   import BoxComponent from './box.vue';
 
 export default {
@@ -17,31 +15,16 @@ export default {
   components: { BoxComponent },
   template: '<box-component>',
   data () {
-    return {};
-  },
-  created: function () {
+    return {count: 1};
   },
   methods: {
+    add_component: function () {
+      this.count++;
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-/*#mydiv {*/
-  /*position: absolute;*/
-  /*z-index: 9;*/
-  /*background-color: #f1f1f1;*/
-  /*border: 1px solid #d3d3d3;*/
-  /*text-align: center;*/
-/*}*/
-
-/*#mydivheader {*/
-  /*padding: 10px;*/
-  /*cursor: move;*/
-  /*z-index: 10;*/
-  /*background-color: #2196F3;*/
-  /*color: #fff;*/
-/*}*/
 </style>
