dbc53a3bcb48ccd5fd9d1590e2fc8fe506653cd8
eretica <eretica.m@gmail.com>
2018-06-06 00:40:38 +0900

    Fix v-for directive

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <button @click="add_component">Add Component</button>
 
-    <box-component v-for="n in count"
+    <box-component v-for="n in count" :key="n"
                    :ini_name="'Component No.' + n"
                    :ini_top="300 + 10.5 * n"
                    :ini_left="10 + 10.5 * n">
